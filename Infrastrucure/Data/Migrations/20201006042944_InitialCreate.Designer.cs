56b7c47e5e1a7f82bfa0e4db80b16427d9e26cf2
Anders Djerf <37343667+istorp@users.noreply.github.com>
2020-10-06 08:34:56 +0200

    End oo day 3 I dont like my port

--- Infrastrucure/Data/Migrations/20201006042944_InitialCreate.Designer.cs
+++ Infrastrucure/Data/Migrations/20201006042944_InitialCreate.Designer.cs
@@ -2,14 +2,16 @@
 using Infrastrucure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastrucure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20201006042944_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
